82f1da9263f2edb612a7c771e7d8c0de143c5626
Robb Jullian <robbjullian730@gmail.com>
2025-11-10 17:25:43 +0800

    working yung history pa lang

--- app/src/main/java/com/example/bulsustudenthandbook/History.java
+++ app/src/main/java/com/example/bulsustudenthandbook/History.java
@@ -3,8 +3,7 @@ package com.example.bulsustudenthandbook;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,28 +11,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class History extends AppCompatActivity {
 
-    LinearLayout history;
+    ImageButton back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_history);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        history = findViewById(R.id.historyBtn);
+        back = findViewById(R.id.backButton);
 
-        history.setOnClickListener(new View.OnClickListener() {
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, History.class);
+                Intent intent = new Intent(History.this, MainActivity.class);
                 startActivity(intent);
             }
         });
